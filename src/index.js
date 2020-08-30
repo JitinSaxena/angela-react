c23ae3ed5e18e2c84e79b5fd2573ee7bb257c4fa
Jitin Saxena <jitinsaxenaaloysian@gmail.com>
2020-08-30 18:52:50 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDom from "react-dom";
+import "../public/styles.css";
+
+ReactDom.render(
+  <div>
+    <h3> hello world</h3>
+    <div>
+      <img src="https://www.google.com/imgres?imgurl=https%3A%2F%2Fcdn.pixabay.com%2Fphoto%2F2016%2F12%2F26%2F17%2F28%2Ffood-1932466__340.jpg&imgrefurl=https%3A%2F%2Fpixabay.com%2Fimages%2Fsearch%2Ffood%2F&tbnid=M40NO-FH258FbM&vet=12ahUKEwip06e0q8LrAhVUcX0KHUnLAGoQMygAegUIARCvAQ..i&docid=XCRvR8qCmFlguM&w=513&h=340&q=food%20ima&ved=2ahUKEwip06e0q8LrAhVUcX0KHUnLAGoQMygAegUIARCvAQ"></img>
+    </div>
+  </div>,
+  document.getElementById("root")
+);
